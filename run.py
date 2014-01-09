5db027e1c7f2aa2b67204d68c2672a181ff2a42a
Karsten Bock <KarstenBock@gmx.net>
2014-01-09 14:13:13 +0100

    Added the command line option "fife_path", which can be used to alter the path where fife should be searched.

--- run.py
+++ run.py
@@ -3,28 +3,38 @@ PyChanEditor Copyright (C) 2014 Karsten Bock
 
 This program is free software; you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free Software
-Foundation; either version 2 of the License, or (at your option) any later
+Foundation; either version 2 of the License, or (at your option) any later 
 version.
 
-This program is distributed in the hope that it will be useful, but WITHOUT
+This program is distributed in the hope that it will be useful, but WITHOUT 
 ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
 FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License along with
-this program; if not, write to the Free Software Foundation, Inc.,
+this program; if not, write to the Free Software Foundation, Inc., 
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 """
 
 import os
+import sys
+import argparse
 
+
+parser = argparse.ArgumentParser(description='PyChanEditor')
+parser.add_argument('--fife_path', type=str, default=None,
+                   help='the path to the fife module')
+args = parser.parse_args()
+if args.fife_path:
+    sys.path.insert(0, args.fife_path)
 from fife import fife
+print ("Using the FIFE python module found here: ",
+        os.path.dirname(fife.__file__))
+
 from fife.extensions.fife_settings import Setting
 
 
 from editor.application import EditorApplication
 
-print ("Using the FIFE python module found here: ",
-        os.path.dirname(fife.__file__))
 
 TDS = Setting(app_name="PyChanEditor", settings_file="./settings.xml")
 
